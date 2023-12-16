7b9d17b79bc96838032d207c9962f558c7c7cde7
rizu06 <riteshsikha6@gmail.com>
2023-12-16 22:19:42 +0530

    rizu

---
+++ src/buttons/Buttons.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import styles from "./buttons.module.css";
+import Button from "./Button";
+import { IconButton } from "../styled";
+import { useDispatch, useSelector } from "react-redux";
+import { setModal } from "../store/appSlice";
+
+const Buttons = () => {
+  const dispatch = useDispatch();
+
+  const store = useSelector((store) => store.app);
+
+  return (
+    <div className={styles.box}>
+      <div className={styles.heading}>
+        <h1>Pocket Notes</h1>
+      </div>
+      <div className={styles.buttons}>
+      {store.buttons.map((m) => (
+          // Add a null check for the 'id' property
+          m && m.id !== null ? (
+            <Button key={m.id} id={m.id} name={m.name} color={m.color} />
+          ) : null
+        ))}
+      </div>
+      <div className={styles.create}>
+        <IconButton
+          $size="4rem"
+          $bgColor="#16008B"
+          onClick={() => dispatch(setModal())}
+        >
+          +
+        </IconButton>
+      </div>
+    </div>
+  );
+};
+
+export default Buttons;
